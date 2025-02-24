854e074096e0fb8379372c4a1314ba4c99fcd5a0
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-02-23 20:31:28 -0700

    Use Fluent Builder Inheritance with Recursive Generics to fix the issue

--- Fluent_Builder_Inheritance_With_Recursive_Generics/Program.cs
+++ Fluent_Builder_Inheritance_With_Recursive_Generics/Program.cs
@@ -6,41 +6,63 @@
     {
         public string Name;
         public string Position;
+
+        public class Builder : PersonJobBuilder<Builder>
+        {
+
+        }
+        public static Builder New => new Builder();
         public override string ToString()
         {
             return $"{nameof(Name)}: {Name}, {nameof(Position)}: {Position}";
         }
     }
 
-    public class PersonInfoBuilder
+    public abstract class PersonBuilder
     {
         protected Person person = new Person(); //protected instead of private because we are using inheritance
-
+        public Person Build()
+        {
+            return person;
+        }
+    }
+    //class Foo : Bar<Foo>
+    public class PersonInfoBuilder<SELF> : PersonBuilder 
+        where SELF : PersonInfoBuilder<SELF>
+    {
+        //we need to restrict <SELF> to ensure nothing unrelevant is passed
         //fluent method
-        public PersonInfoBuilder Called(string name)
+        public SELF Called(string name)
         {
             person.Name = name;
-            return this;
+            //cast manually
+            return (SELF)this;
         }
     }
 
     //imagine you need to add additional info but don't want to chnage existing class
-    public class PersonJobBuilder : PersonInfoBuilder 
+    //use generic argument <SELF>
+    public class PersonJobBuilder<SELF> : PersonInfoBuilder<PersonJobBuilder<SELF>> 
+        where SELF : PersonJobBuilder<SELF>
     {
-        public PersonJobBuilder WorksAsA(string position)
+        public SELF WorksAsA(string position)
         {
             person.Position = position;
-            return this;
+            //cast manually
+            return (SELF)this;
         }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            var builder = new PersonJobBuilder();
+            //var builder = new PersonJobBuilder();
             //builder.Called("Yoko").WorksAsA("position");
             //=> not working as Called() returns PersonInfoBuilder, not PersonJobBuilder
             //and PersonInfoBuilder doesn't know about WorksAsA()
+
+            var person = Person.New.Called("Yoko").WorksAsA("quant").Build();
+            Console.WriteLine(person);
         }
     }
 }
