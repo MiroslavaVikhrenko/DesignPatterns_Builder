1cf80a0c4459bc144a8af22e9c64426ded6bdabf
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-02-23 20:00:05 -0700

    Create Person class

---
+++ Fluent_Builder_Inheritance_With_Recursive_Generics/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Fluent_Builder_Inheritance_With_Recursive_Generics
+{
+    //when builders inherit from other builders
+
+    public class Person
+    {
+        public string Name;
+        public string Position;
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Position)}: {Position}";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
