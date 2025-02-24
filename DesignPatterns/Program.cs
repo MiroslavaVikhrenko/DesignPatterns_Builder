e1eec0ddd080441137a89f8a3106c8feacf2a9c7
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-02-23 19:20:32 -0700

    Try 'Life without builder' scenario, using stringbuilder for building html elements

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace DesignPatterns
+﻿using System.Text;
+
+namespace DesignPatterns
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            var hello = "hello";
+            var sb = new StringBuilder();
+            sb.Append("<p>");
+            sb.Append(hello);
+            sb.Append("</p>");
+            Console.WriteLine(sb);
+
+            var words = new[] { "hello", "world" };
+            sb.Clear();
+            sb.Append("<ul>");
+            foreach (var word in words)
+            {
+                sb.AppendFormat($"<li>{word}</li>");
+            }
+            sb.Append("</ul>");
+            Console.WriteLine(sb);
+
         }
     }
 }
