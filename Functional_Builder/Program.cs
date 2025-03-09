97e2479bb4ce947cc89ec27831f392b26e826889
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-03-09 14:46:25 -0600

    Practice Functional builder

---
+++ Functional_Builder/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Functional_Builder
+{
+    // Functional builder 
+    internal class Program
+    {
+        public class Person
+        {
+            public string Name, Position;
+        }
+        // sealed class => you cannot inherit from it
+        // => if you want to extend it somehow - you cannot use inheritance 
+        // but you can use extension methods instead
+        public sealed class PersonBuilder
+        {
+            //<Person, Person> => fluent 
+            // we are going to affect the person somehow and we're going to return a reference to that person
+            private readonly List<Func<Person, Person>> actions
+                = new List<Func<Person, Person>>();
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
