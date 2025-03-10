143b88436ba16aa5f6d601b826ac57e0924f37b2
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-03-09 18:12:22 -0600

    Add 3 interfaces (ISpecifyCarType, ISpecifyWheelSize and IBuildCar) to follow Interface segregation principle

--- Stepwise_Builder/Program.cs
+++ Stepwise_Builder/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace Stepwise_Builder
 {
-    // Stepwise Builder 
+    // Stepwise Builder + Interface segregation principle
     // How do you actually get the builder to perform a set of steps?
     // (one after another in a specific order)
 
@@ -18,7 +18,28 @@
         // Sedan: 15 to 17 in
         // Crossover: 17 to 20 in
         // This means that if you want to use builder to construct a car => you have to construct it in a specific order
+        // First specify the type and after that specify the wheel size and maybe apply some validation
+
+        // 1. return one interface for onfiguring the type,
+        // 2. then another for wheel size
+        // 3. and then the 3rd one for actually building the Car
     }
+
+    public interface ISpecifyCarType
+    {
+
+    }
+
+    public interface ISpecifyWheelSize
+    {
+
+    }
+
+    public interface IBuildCar
+    {
+
+    }
+
     internal class Program
     {
         static void Main(string[] args)
